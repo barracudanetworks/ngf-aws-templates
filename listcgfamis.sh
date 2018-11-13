4e5032fe8cf249a6ff790ae7011ab896f3d23738
Michael Zoller <32059378+mzoller-cuda@users.noreply.github.com>
2018-11-13 14:49:01 +0100

    * 8.0.0 changes * cleanup of old AMIs * added REST API condition to UserData and Template * Added 8.0.0 as the default image * Replaced AWS instance types to match 8.0.0 * added signaling to avoid the instance being shown as ready in the portal when the scripts have not finished running

---
+++ listcgfamis.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#PAYG='https://aws.amazon.com/marketplace/pp/B010GKMEKW'
+#BYOL='https://aws.amazon.com/marketplace/pp/B014GECC1A'
+#Metered='https://aws.amazon.com/marketplace/pp/B077G9FKK7'
+
+#set the version and build number and then run the script
+VERSION='800-0475'
+
+ami_regions=($(aws ec2 describe-regions --output text | sed -e 's/^.*com[[:blank:]]//'))
+
+
+num_regions=${#ami_regions[@]}
+
+for i in "${ami_regions[@]}"
+do
+	filter='Name=name,Values=CudaCGFHourly-v'"$VERSION"'*'
+	PAYGAMI=$(aws ec2 describe-images --owner aws-marketplace --region $i --filters $filter --query 'Images[*].{ID:ImageId}' --output text)
+	echo "\"$i\" : {"
+	echo "    \"Hourly\" : \"$PAYGAMI\","
+
+	filter='Name=name,Values=CudaCGFBYOL-v'"$VERSION"'*'
+
+	BYOLAMI=$(aws ec2 describe-images --owner aws-marketplace --region $i --filters $filter --query 'Images[*].{ID:ImageId}' --output text)
+	echo "    \"BYOL\" : \"$BYOLAMI\","
+
+	filter='Name=name,Values=CudaCGFMetered*v'"$VERSION"'*'
+
+	METEREDAMI=$(aws ec2 describe-images --owner aws-marketplace --region $i --filters $filter --query 'Images[*].{ID:ImageId}' --output text)
+	echo "    \"Metered\" : \"$METEREDAMI\""
+	if [[ $i == ${ami_regions[${#ami_regions[@]}-1]} ]]
+	then
+		echo "}"
+	else
+        	echo "},"
+	fi
+
+done
